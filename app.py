1f2580a3a20e60f0a6505bd6f970693f7414cff6
yehee0820 <yehee0820@ewhain.net>
2020-09-12 11:46:09 +0900

    final

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 import os
 
 app = Flask(__name__)
-client = MongoClient('mongodb://test:test@13.125.102.200', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.legohangang  # 'dbsparta'라는 이름의 db를 만듭니다.
 
 
@@ -303,6 +303,6 @@ def bike_origin():
 
 # 실행
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('localhost', port=5000, debug=True)
 
 
